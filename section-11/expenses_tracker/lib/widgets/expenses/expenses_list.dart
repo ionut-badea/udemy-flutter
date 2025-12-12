901c6cb234ed0dc8c85ad1aeaa75540e9191a981
Ionut Badea <ionut.badea@yazee.me>
2025-12-12 14:48:14 +0200

    section-11 | app init
    Signed-off-by: Ionut Badea <ionut.badea@yazee.me>

---
+++ section-11/expenses_tracker/lib/widgets/expenses/expenses_list.dart
@@ -0,0 +1,32 @@
+import 'package:expenses_tracker/model/expense.dart';
+import 'package:expenses_tracker/widgets/expenses/expense_item.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+    required this.removeExpense,
+  });
+
+  final List<Expense> expenses;
+  final void Function(Expense expense) removeExpense;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error,
+          margin: EdgeInsets.symmetric(
+            horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+          ),
+        ),
+        onDismissed: (direction) => removeExpense(expenses[index]),
+        child: ExpenseItem(expenses[index]),
+      ),
+    );
+  }
+}
